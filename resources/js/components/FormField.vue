a44d16474a90f5fece1b6c785b5094e584601e0a
JapSeyz <JapSeyz@JapSeyz.com>
2018-09-10 19:44:33 +0200

    Adds currency to the form-field

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,12 +1,16 @@
 <template>
     <default-field :field="field">
         <template slot="field">
-            <input :id="field.name" type="text"
-                class="w-full form-control form-input form-input-bordered"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value"
-            />
+            <div class="flex items-center">
+                <span class="mr-1">{{ field.currency }}</span>
+            
+                <input :id="field.name" type="text"
+                    class="w-full form-control form-input form-input-bordered flex-1"
+                    :class="errorClasses"
+                    :placeholder="field.name"
+                    v-model="value"
+                />
+            </div>
 
             <p v-if="hasError" class="my-2 text-danger">
                 {{ firstError }}
