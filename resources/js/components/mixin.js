6902cf0845cdc3f34b103b7976a83da07b8a9c1c
Talha Zekeriya Durmuş <drtzack@gmail.com>
2018-11-16 03:46:19 +0300

    Update mixin.js

--- resources/js/components/mixin.js
+++ resources/js/components/mixin.js
@@ -1,6 +1,7 @@
 export default {
     computed: {
         formattedValue() {
+            if (!this.field.value) return '';
             return this.field.value.toLocaleString(this.field.locale, {
                 style: 'currency',
                 currency: this.field.currency,
