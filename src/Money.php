f310add5469855da6c30db9b967829a2b39645be
Talha Zekeriya Durmuş <drtzack@gmail.com>
2018-11-16 03:46:58 +0300

    Update Money.php

--- src/Money.php
+++ src/Money.php
@@ -2,6 +2,8 @@
 
 namespace Vyuldashev\NovaMoneyField;
 
+use Money\Currencies\AggregateCurrencies;
+use Money\Currencies\BitcoinCurrencies;
 use Money\Currency;
 use Laravel\Nova\Fields\Number;
 use Money\Currencies\ISOCurrencies;
@@ -59,7 +61,10 @@ class Money extends Number
 
     public function subUnits(string $currency)
     {
-        return (new ISOCurrencies())->subunitFor(new Currency($currency));
+        return (new AggregateCurrencies([
+            new ISOCurrencies(),
+            new BitcoinCurrencies(),
+        ]))->subunitFor(new Currency($currency));
     }
 
     public function minorUnit($currency)
