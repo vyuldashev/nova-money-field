00a1178b3ac915843b840cc1ec3d85fe41291a3a
Talha Zekeriya Durmuş <drtzack@gmail.com>
2018-12-10 16:56:35 +0300

    maximumFractionDigits relative to subUnits

--- resources/js/components/mixin.js
+++ resources/js/components/mixin.js
@@ -1,12 +1,12 @@
 export default {
     computed: {
         formattedValue() {
-            if (!this.field.value) return '';
+            if (this.field.value === undefined) return '';
             return this.field.value.toLocaleString(this.field.locale, {
                 style: 'currency',
                 currency: this.field.currency,
                 minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+                maximumFractionDigits: this.field.subUnits
             });
         },
     }
